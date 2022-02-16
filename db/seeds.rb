9adc7642e16e1b1de2cfd22bf2b8e244331fd8ca
Ida Schwarzkopf <ida.schwarzkopf@ha-es.de>
2022-02-16 16:05:48 +0100

    index, show, new, create, edit, update works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Flat.destroy_all
+
+5.times do
+  Flat.create!(
+    name: Faker::Movies::LordOfTheRings.character,
+    address: Faker::Movies::LordOfTheRings.location,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: (rand(20..200)),
+    number_of_guests: rand(0..20)
+  )
+end
